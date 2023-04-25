8f09266c10a3e8e83697f6d60a9d51063e4e704c
Kevin Price <collegekevin@gmail.com>
2023-04-25 10:46:51 -0700

    commit tree e7466c6bee8e9295ef4d5d0dc85cd34f38f5b9a1

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution here!
+// // Write your solution here! 
+  
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+    
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+    return cats;
+}
+
+function appendCat(name){
+    const newLastCat = [...cats, (name)];
+    return newLastCat;
+}
+
+function prependCat(name){
+    const newFirstCat = [(name), ...cats];
+    return newFirstCat;
+}
+
+function removeLastCat(){
+    const lostLastCat = cats.slice(0, -1);
+    return lostLastCat;
+}
+
+function removeFirstCat(){
+    const lostFirstCat = cats.slice(1);
+    return lostFirstCat;
+}
+
+
+
+
+
+
+
